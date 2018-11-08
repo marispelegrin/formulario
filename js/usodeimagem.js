966ec0e81bb46f9af7b6d91f75c861f106a48ccb
marispelegrin <43457630+marispelegrin@users.noreply.github.com>
2018-11-08 21:39:57 -0200

    Update usodeimagem.js

--- js/usodeimagem.js
+++ js/usodeimagem.js
@@ -90,3 +90,19 @@ function registrarNomeEmpresa() {
         }
     });
 }
+function registrarNomeAgente() {
+	var statusTransacao = document.getElementById("statusTransacaoNomeEmpresa");
+	var nomeAgente = document.formNomeAgente.campoNomeAgente.value;
+	statusTransacao.innerHTML = "Enviando transação. Por favor monitore seu Metamask.";
+	contratoUsoDeImagem.definirNomeDoAgente(nomeAgente, {from: contaUsuario, gas: 3000000, value: 0}, function (err, resultado) {
+        if (err)    {
+            console.log("Erro");
+			console.error(err);
+			statusTransacao.innerHTML = "Erro: " + err;
+        } else {
+            console.log("Resultado");
+            console.log(resultado);
+            statusTransacao.innerHTML = "Transação enviada ao Blockchain Ethereum. Faça a monitoração pelo hash: " + resultado;
+        }
+    });
+}
